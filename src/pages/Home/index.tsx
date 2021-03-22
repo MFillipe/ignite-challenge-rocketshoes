d854b89dd406169f93c83ff233eba49a04c7a594
Matheus Filipe <matheus_filipe00@outlook.com>
2021-03-22 20:16:05 -0300

    feat: Add total, remove and update products

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,44 +25,43 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-     const response = await api.get('/products');
-     const productFormated = response.data.map(function(product: Product) {
-       return {
-         ...product, 
-         price: formatPrice(product.price)
-       }
-     })
-     setProducts(productFormated);
+      const response = await api.get('/products');
+      const productsFormated = response.data.map(function(product: Product){
+        return {...product, price: formatPrice(product.price)}
+      })
+      setProducts(productsFormated)
+      
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    addProduct(id);
+    addProduct(id)
   }
 
   return (
     <ProductList>
-      {products.map(product => (
+      {products.map((product) => (
         <li key={product.id}>
-        <img src={product.image} alt={product.title}/>
+        <img src={product.image} alt={product.title} />
         <strong>{product.title}</strong>
         <span>{product.price}</span>
         <button
           type="button"
           data-testid="add-product-button"
-         onClick={() => handleAddProduct(product.id)}
+          onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
+            {cartItemsAmount[product.id] || 0}
           </div>
 
           <span>ADICIONAR AO CARRINHO</span>
